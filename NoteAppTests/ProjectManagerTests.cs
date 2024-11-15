7e731204c8f2e319c8b22f12b304c53fd0ed29a0
Sergey Akimov <superuser.superuser24@gmail.com>
2024-11-15 19:15:22 +0300

    add business logic tests;

---
+++ NoteAppTests/ProjectManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using NoteApp.Extensions;
+using NoteApp.Models;
+using NoteApp.Services;
+
+namespace NoteAppTests;
+
+internal class ProjectManagerTests : TestsBase
+{
+    [Test]
+    public void ShouldSerializeAndSaveProject()
+    {
+        var testProject = GetTestProject();
+
+        ProjectManager.Save(testProject);
+
+        File.Exists(ProjectManager.PROJECT_LOCATION).Should().BeTrue();
+        var result = File.ReadAllText(ProjectManager.PROJECT_LOCATION).FromJson<Project>();
+        result.Should().BeEquivalentTo(testProject);
+    }
+
+    [Test]
+    public void ShouldLoadAndDeserializeProject()
+    {
+        var testProject = GetTestProject();
+        ProjectManager.Save(testProject);
+
+        var result = ProjectManager.Load();
+        result.Should().BeEquivalentTo(testProject);
+    }
+
+    [Test]
+    public void ShouldThrowIfProjectFileCorrupted()
+    {
+        File.WriteAllText(ProjectManager.PROJECT_LOCATION, "wrong");
+        Assert.Throws<JsonReaderException>(() => ProjectManager.Load());
+    }
+}
